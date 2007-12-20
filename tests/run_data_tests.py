dba4b8376c2ed9d2a012aeb17d0eb48db20eba8a
api.jscudder <devnull@localhost>
2007-12-20 01:39:20 +0000

    Added codesearch tests to the run data tests script.

--- tests/run_data_tests.py
+++ tests/run_data_tests.py
@@ -15,6 +15,7 @@ import gdata_tests.docs_test
 import gdata_tests.spreadsheet_test
 import gdata_tests.auth_test
 import gdata_tests.photos_test
+import gdata_tests.codesearch_test
 
 
 def RunAllTests():
@@ -22,7 +23,8 @@ def RunAllTests():
   test_runner.modules = [gdata_test, atom_test, gdata_tests.apps_test, 
                          gdata_tests.base_test, gdata_tests.calendar_test, 
                          gdata_tests.docs_test, gdata_tests.spreadsheet_test,
-                         gdata_tests.auth_test, gdata_tests.photos_test]
+                         gdata_tests.auth_test, gdata_tests.photos_test,
+                         gdata_tests.codesearch_test]
   test_runner.RunAllTests()
   
 if __name__ == '__main__':
