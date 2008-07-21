b22dc01482c348b385b52ecf5e13ecd892bc652d
api.jscudder <devnull@localhost>
2008-07-21 23:17:52 +0000

    Added comments to better describe the use of the list entry's custom dictionary in the spreadsheets sample.

--- samples/spreadsheets/spreadsheetExample.py
+++ samples/spreadsheets/spreadsheetExample.py
@@ -145,8 +145,11 @@ class SimpleCRUD:
         print '%s %s\n' % (entry.title.text, entry.content.text)
       elif isinstance(feed, gdata.spreadsheet.SpreadsheetsListFeed):
         print '%s %s %s' % (i, entry.title.text, entry.content.text)
+        # Print this row's value for each column (the custom dictionary is
+        # built using the gsx: elements in the entry.)
+        print 'Contents:'
         for key in entry.custom:  
-          print '%s: %s' % (key, entry.custom[key].text) 
+          print '  %s: %s' % (key, entry.custom[key].text) 
         print '\n',
       else:
         print '%s %s\n' % (i, entry.title.text)
