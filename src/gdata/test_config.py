717eead0da70fb72d573c23d6b981e3c2026d6dd
j.s@google.com <j.s@google.com>
2009-03-28 00:00:35 +0000

    Moving MockHttpClient configuration logic out of live_client_test and into the more general test_config source file.

--- src/gdata/test_config.py
+++ src/gdata/test_config.py
@@ -14,7 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import gdata.test_config_template
+
 
 """Loads configuration for tests which connect to Google servers.
 
@@ -24,4 +26,80 @@ the tests which communicate with the servers. Change the import above and
 settings assignment below to use your own test configuration.
 """
 
+
 settings = gdata.test_config_template
+
+
+def configure_client(client, config, case_name):
+  """Sets up a mock client which will reuse a saved session.
+
+  Should be called during setUp of each unit test.
+
+  Handles authentication to allow the GDClient to make requests which
+  require an auth header.
+
+  Args:
+    client: a gdata.GDClient whose http_client member should be replaced
+            with a atom.mock_http_core.MockHttpClient so that repeated
+            executions can used cached responses instead of contacting
+            the server.
+    config: a dict of test specific settings from the gdata.test_config
+            module. Examples can be found in gdata.test_config_template
+            look at BloggerConfig and ContactsConfig.
+    case_name: str The name of the test case class. Examples: 'BloggerTest',
+               'ContactsTest'. Used to save a session
+               for the ClientLogin auth token request, so the case_name
+               should be reused if and only if the same username, password,
+               and service are being used.
+  """
+  # Use a mock HTTP client which will record and replay the HTTP traffic
+  # from these tests.
+  client.http_client = atom.mock_http_core.MockHttpClient()
+  client.http_client.cache_case_name = case_name
+  # Getting the auth token only needs to be done once in the course of test
+  # runs.
+  if config.auth_token is None:
+    client.http_client.cache_test_name = 'client_login'
+    cache_name = client.http_client.get_cache_file_name()
+    if settings.CLEAR_CACHE:
+      client.http_client.delete_session(cache_name)
+    client.http_client.use_cached_session(cache_name)
+    config.auth_token = client.request_client_login_token(
+        config.email(), config.password(), config.service, case_name)
+    client.http_client.close_session()
+  client.auth_token = config.auth_token
+
+
+def configure_cache(client, test_name):
+  """Loads or beings a cached session to record HTTP traffic.
+
+  Should be called at the beginning of each test method.
+
+  Args:
+    test_name: str The name of this test method. Examples: 
+               'TestClass.test_x_works', 'TestClass.test_crud_operations'.
+               This is used to name the recording of the HTTP requests and
+               responses, so it should be unique to each test method in the
+               test case.
+  """
+  # Auth token is obtained in configure_client which is called as part of
+  # setUp.
+  client.http_client.cache_test_name = test_name
+  cache_name = client.http_client.get_cache_file_name()
+  if settings.CLEAR_CACHE:
+    client.http_client.delete_session(cache_name)
+  client.http_client.use_cached_session(cache_name)
+
+
+def close_client(client):
+  """Saves the recoded responses to a temp file if the config file allows.
+  
+  This should be called in the unit test's tearDown method.
+
+  Checks to see if settings.CACHE_RESPONSES is True, to make sure we only
+  save sessions to repeat if the user desires.
+  """
+  if settings.CACHE_RESPONSES:
+    # If this was a live request, save the recording.
+    client.http_client.close_session()
+
